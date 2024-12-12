ee302c7a3d703bbeef918b67ddc8bb8407ac6f2c
SikHyoKim <dogdog120@naver.com>
2024-12-12 14:48:50 +0900

    chore: 주석제거, kakao sdk 초기화 로직

--- App.tsx
+++ App.tsx
@@ -2,38 +2,13 @@ import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, ActivityIndicator, StyleSheet} from 'react-native';
 import Router from './src/route/Router';
+import { initializeKakaoSDK } from '@react-native-kakao/core';
+import Config from 'react-native-config';
+
 
 function App(): React.JSX.Element {
-  const [isReady, setIsReady] = useState(false);
-
-  // useEffect(() => {
-  //   const prepare = async () => {
-  //     try {
-  //       // 실제 초기화 작업 수행
-  //       await Promise.all([
-  //         // 예: 폰트 로드
-  //         // 예: 캐시된 데이터 로드
-  //         // 예: 인증 상태 확인
-  //         new Promise(resolve => setTimeout(resolve, 1500)),
-  //       ]);
-  //     } catch (e) {
-  //       console.warn(e);
-  //     } finally {
-  //       setIsReady(true);
-  //       SplashScreen.hide();
-  //     }
-  //   };
-
-  //   prepare();
-  // }, []);
-
-  // if (!isReady) {
-  //   return (
-  //     <View style={styles.loadingContainer}>
-  //       <ActivityIndicator size="large" color="#000000" />
-  //     </View>
-  //   );
-  // }
+  // SDK 초기화
+  initializeKakaoSDK(Config.KAKAO_NATIVE_APP_KEY || '');
 
   return (
       <NavigationContainer>
