4ea745db98e50abfb63faa2e772f0e5b80905852
SikHyoKim <dogdog120@naver.com>
2024-12-30 16:39:32 +0900

    필요 없는 코드 제거

--- src/Pages/login/KaKaoLogin.jsx
+++ src/Pages/login/KaKaoLogin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, View } from 'react-native';
-import { login, isLogined } from '@react-native-kakao/user';
+import { login } from '@react-native-kakao/user';
 import { useNavigation } from '@react-navigation/native';
 
 const KaKaoLogin = () => {
@@ -9,27 +9,16 @@ const KaKaoLogin = () => {
   const handleLogin = async () => {
     try {
       const result = await login();
-      console.log('Login Success', JSON.stringify(result));
-      
-      // 로그인 성공 시 MainTab으로 이동
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'MainTab' }],
-      });
-      
+      console.log('Login Success', result);
+      navigation.navigate('Main');
     } catch (error) {
-      if (error.code === 'E_CANCELLED_OPERATION') {
-        console.log('Login Cancelled', error.message);
-      } else {
-        console.log(`Login Failed (code: ${error.code})`, error.message);
-      }
+      console.error('Login Error:', error);
     }
   };
 
   return (
     <View>
       <Button title="카카오 로그인" onPress={handleLogin} />
-      <Button title="로그인 확인" onPress={() => console.log(isLogined)} />
     </View>
   );
 };
