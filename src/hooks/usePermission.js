64434fe63c0c16bb2a9da54d71ee1e2e67f95e5e
SikHyoKim <dogdog120@naver.com>
2025-01-06 16:20:55 +0900

    ios ruby 버전업,불필요한 권한요청 파일 삭제

--- src/hooks/usePermission.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { PERMISSIONS_TYPE, checkPermission } from '../utils/permissions';
-
-export const usePermission = (type) => {
-  useEffect(() => {
-    checkPermission(type);
-  }, [type]);
-};
