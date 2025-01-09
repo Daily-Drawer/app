62b3fae971d23d9dff646c0d3db4f9d591bd5d36
SikHyoKim <dogdog120@naver.com>
2025-01-09 20:23:25 +0900

    로그아웃 로직 오타 수정

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,11 +4,17 @@ import axios from '../apis/axios';
 
 export const applogout = async (loginType) => {
   try {
-    // 1. 서버에 로그아웃 알림
-    await axios.post('/auth/logout');
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    
+    // 1. 서버에 로그아웃 알림 (올바른 엔드포인트로 수정)
+    await axios.post('/api/v1/auth/logout', null, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
 
     // 2. 로그인 타입에 따른 처리
-    if (loginType === 'KAKAO') {
+    if (loginType === 'kakao') {
       await logout(); // 카카오 SDK 로그아웃
     }
 
@@ -16,16 +22,18 @@ export const applogout = async (loginType) => {
     await AsyncStorage.multiRemove([
       'accessToken',
       'refreshToken',
-      'loginType',
-      'userData',
+      'loginType'
     ]);
-
-    // 4. axios 헤더에서 토큰 제거
-    delete axios.defaults.headers.common['Authorization'];
 
     return true;
   } catch (error) {
     console.error('로그아웃 실패:', error);
+    // 서버 에러가 발생하더라도 로컬 데이터는 삭제
+    await AsyncStorage.multiRemove([
+      'accessToken',
+      'refreshToken',
+      'loginType'
+    ]);
     throw error;
   }
 };
